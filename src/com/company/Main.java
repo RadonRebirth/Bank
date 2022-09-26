ea885c6a9f737ab4cdafb4663cc23ed4a19c6398
радон <jf.com@mail.ru>
2022-09-27 01:11:11 +0300

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,18 +4,19 @@ class Account extends Thread{
         this.name = name;
     }
     String name;
-    double max = 1500;
-    double rnd = Math.random()*250;
-    int balance = (int) -rnd;
+    int max = 1500;
+    int rnd = (int) (Math.random()*250);
+    int balance = -rnd;
     public void run(){
         for(int i = 0;i<max;i+=rnd) {
             balance+=rnd;
-            System.out.println("Баланс "+name+" составляет "+ balance);
+            System.out.println("Баланс "+name+" составляет: "+ balance);
         }
     }
     public void getBalance(){
         if(balance > 1000){
             balance-=500;
+            System.out.println("Списание с счёта "+name+" в размере: 500");
             System.out.println("Остаток счёта "+name+" составляет: "+ balance);
         }
     }
