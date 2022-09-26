01bb32bda31963cef582b327b84387a08af0478c
радон <jf.com@mail.ru>
2022-09-27 02:42:57 +0300

    e

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,31 +1,75 @@
 package com.company;
+
+import java.util.Scanner;
+
 class Account extends Thread{
-    public Account(String name){
-        this.name = name;
-    }
-    String name;
-    int max = 1500;
-    int rnd = (int) (Math.random()*250);
-    int balance = -rnd;
-    public void run(){
-        for(int i = 0;i<max;i+=rnd) {
-            balance+=rnd;
-            System.out.println("Баланс "+name+" составляет: "+ balance);
+    private int money = 0;
+    private int maxMoney = 1500;
+    int minus = 500;
+    public synchronized void setBalance(){
+        while(money >= maxMoney){
+            try{
+                wait();
+            }
+            catch (Exception ex){
+                ex.getMessage();
+            }
         }
+        money += 200;
+        System.out.println("Пополнение баланса на 200 \n Остаток баланcа: " + money);
+        notify();
     }
-    public void getBalance(){
-        if(balance > 1000){
-            balance-=500;
-            System.out.println("Списание с счёта "+name+" в размере: 500");
-            System.out.println("Остаток счёта "+name+" составляет: "+ balance);
+    public synchronized void getMoney(){
+        while(money < maxMoney){
+            try {
+                wait();
+            }
+            catch (Exception ex){
+                ex.getMessage();
+            }
+        }
+        System.out.println("Введите сумму вывода: ");
+        Scanner in = new Scanner(System.in);
+        minus = in.nextInt();
+        money -= minus;
+        if(money < 0){
+            System.out.println("На балансе нет такой суммы");
+            System.exit(0);
+        }
+        System.out.println("Произошло списание в размере " + minus + "\n Остаток баланса: " + money);
+        notify();
+    }
+}
+
+class SetBalance extends Thread{
+    Account account;
+    SetBalance(Account account){
+        this.account=account;
+    }
+    public void run(){
+        for (int i = 1; i < 50; i++) {
+            account.setBalance();
         }
     }
 }
-public class Main{
-    public static void main(String[] args) throws InterruptedException {
-	Account account = new Account("RaDoN");
-    account.start();
-    account.join();
-    account.getBalance();
+class GetMoney extends Thread{
+
+    Account account;
+    GetMoney(Account account){
+        this.account=account;
+    }
+    public void run(){
+        for (int i = 1; i < 50; i++) {
+            account.getMoney();
+        }
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Account account = new Account();
+        GetMoney setBal = new GetMoney(account);
+        SetBalance getMon = new SetBalance(account);
+        new Thread(setBal).start();
+        new Thread(getMon).start();
     }
 }
